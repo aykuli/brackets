2ffed071927718049656a5337744d90bf396878c
Aykuli <aykuli@ya.ru>
2019-10-02 22:07:43 +0700

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [str[0]];
+    let res = true;
+
+    for (let i = 1; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                if (
+                    stack[stack.length - 1] == bracketsConfig[j][1] &&
+                    stack[stack.length - 1] == bracketsConfig[j][0]
+                ) {
+                    stack.pop();
+                } else {
+                    stack.push(str[i]);
+                }
+            } else if (str[i] == bracketsConfig[j][1]) {
+                if (stack[stack.length - 1] == bracketsConfig[j][0]) {
+                    stack.pop();
+                } else {
+                    stack.push(str[i]);
+                }
+            }
+        }
+    }
+    if (stack.length == 0) {
+        res = true;
+    } else {
+        res = false;
+    }
+
+    return res;
+};
